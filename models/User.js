17685a3c7f2fd89c4468191d39d9cd3d664feda4
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-08-20 14:35:33 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
-const botSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  repoUrl: { type: String, required: true },
-  description: { type: String },
-  logoUrl: { type: String },
-  owner: { type: String, required: true }
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false }
 });
 
-export default mongoose.model("Bot", botSchema);
+export default mongoose.model("User", userSchema);

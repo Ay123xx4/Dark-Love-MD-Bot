050a72dc0eb6a734b4a3055f60278f6d50323154
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-08-22 14:03:04 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  verified: { type: Boolean, default: false }
+  isVerified: { type: Boolean, default: false },
+  verificationCode: { type: String }
 });
 
-export default mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
+
